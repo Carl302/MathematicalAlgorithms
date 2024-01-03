c41f3a154d89085ca16bf5f99a3c5ba5be538959
lubaojun <103750121@qq.com>
2024-01-03 10:44:27 +0800

    添加项目文件。

---
+++ TheSumOfTwoNumbers.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MathematicalAlgorithms
+{
+    /// <summary>
+    /// 两数之和 的交互逻辑
+    /// </summary>
+    public partial class TheSumOfTwoNumbers : Window
+    {
+        public TheSumOfTwoNumbers()
+        {
+            InitializeComponent();
+        }
+        public int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+            return new int[0];
+        }
+    }
+}
